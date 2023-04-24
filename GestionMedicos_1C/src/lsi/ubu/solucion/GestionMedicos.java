723616c7b059205b8b0bd9a63d8c5d0c945fb16b
ecl1009 <ecl1009@alu.ubu.es>
2023-04-24 20:50:50 +0200

    Primera versión de reservar_consulta y parte de anullar
    Se añade la primera versión de reservar_consulta (No quedó grabada en el anterior commit) y parte de anular_consulta

--- GestionMedicos_1C/src/lsi/ubu/solucion/GestionMedicos.java
+++ GestionMedicos_1C/src/lsi/ubu/solucion/GestionMedicos.java
@@ -14,27 +14,26 @@ import lsi.ubu.enunciado.GestionMedicosException;
 import lsi.ubu.util.ExecuteScript;
 import lsi.ubu.util.PoolDeConexiones;
 
-
 /**
- * GestionMedicos:
- * Implementa la gestion de medicos segun el PDF de la carpeta enunciado
+ * GestionMedicos: Implementa la gestion de medicos segun el PDF de la carpeta
+ * enunciado
  * 
- * @author <a href="mailto:ecl1009@alu.ubu.es">Eduardo Manuel Cabeza Lopez</a> 
+ * @author <a href="mailto:ecl1009@alu.ubu.es">Eduardo Manuel Cabeza Lopez</a>
  * @version 1.0
- * @since 1.0 
+ * @since 1.0
  */
 public class GestionMedicos {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(GestionMedicos.class);
 
 	private static final String script_path = "sql/";
 
-	public static void main(String[] args) throws SQLException{		
+	public static void main(String[] args) throws SQLException {
 		tests();
 
 		System.out.println("FIN.............");
 	}
-	
+
 	public static void reservar_consulta(String m_NIF_cliente, 
 			String m_NIF_medico,  Date m_Fecha_Consulta) throws SQLException {
 		
@@ -42,111 +41,174 @@ public class GestionMedicos {
 		Connection con=null;
 		PreparedStatement pst_sel_cliente = null;
 		PreparedStatement pst_sel_medico = null;
+		PreparedStatement pst_sel_consulta = null;
+		PreparedStatement pst_sel_anulacion = null;
+		PreparedStatement pst_ins_consulta = null;
+		PreparedStatement pst_upd_medico = null;
 		ResultSet rs_sel_cliente = null;
 		ResultSet rs_sel_medico = null;
+		ResultSet rs_sel_consulta = null;
+		ResultSet rs_sel_anulacion = null;
 
 	
 		try{
 			con = pool.getConnection();
+			con.setAutoCommit(false);
 			pst_sel_cliente =  con.prepareStatement(
-					"select NIF from cliente where NIF=?");
+					"select NIF "
+					+ "from cliente "
+					+ "where NIF=?");
 			pst_sel_cliente.setString(1,m_NIF_cliente);
 			rs_sel_cliente = pst_sel_cliente.executeQuery();
 			if(!rs_sel_cliente.next()) {
 				throw new GestionMedicosException(1);
 			}
+			
 			pst_sel_medico = con.prepareStatement(
-					"select NIF from medico where NIF=?");
+					"select NIF "
+					+ "from medico "
+					+ "where NIF=?");
 			pst_sel_medico.setString(1,m_NIF_medico);
 			rs_sel_medico = pst_sel_medico.executeQuery();
 			if(!rs_sel_medico.next()) {
 				throw new GestionMedicosException(2);
 			}
+			// Un médico solo puede tener una consulta en un mismo día.
+			// La siguiente sentencia preparada devolverá a lo sumo una fila.
+			pst_sel_consulta = con.prepareStatement(
+					"select id_consulta, id_medico "
+					+ "from "
+					+ "(select * "
+					+ "from consulta join medico "
+					+ "on NIF = ? "
+					+ "and medico.id_medico = consulta.id_medico "
+					+ "and fecha_consulta = ?) c");
+			java.sql.Date m_Fecha_sql = new java.sql.Date(m_Fecha_Consulta.getTime());
+			pst_sel_consulta.setDate(2, m_Fecha_sql);
+			pst_sel_consulta.setString(1, m_NIF_medico);
+			rs_sel_consulta = pst_sel_consulta.executeQuery();
+			if(rs_sel_consulta.next()) {
+				//Por si existe anulación de la consulta
+				pst_sel_anulacion = con.prepareStatement(
+						"select id_anulacion "
+						+ "from anulacion "
+						+ "where id_consulta = ?");
+				rs_sel_consulta.first(); // Asegurar que apuntamos a la primera y única fila.
+				pst_sel_anulacion.setInt(1, rs_sel_consulta.getInt(1));
+				rs_sel_anulacion = pst_sel_anulacion.executeQuery();
+				if(!rs_sel_anulacion.next()) {
+					throw new GestionMedicosException(3);
+				}
+			}
 			
-								
+			pst_ins_consulta = con.prepareStatement(
+					"insert into consulta "
+					+ "values (seq_consulta.nextval(), ?, ?, ?)");
+			pst_ins_consulta.setDate(1, m_Fecha_sql);
+			pst_ins_consulta.setInt(2, rs_sel_consulta.getInt(2));
+			pst_ins_consulta.setString(3, m_NIF_cliente);
+			pst_ins_consulta.executeUpdate();
+			
+			pst_upd_medico = con.prepareStatement(
+					"update medico"
+					+ "set consultas = consultas + 1"
+					+ "where NIF = ?");
+			pst_upd_medico.setString(1, m_NIF_medico);
+			pst_upd_medico.executeUpdate();
+			con.commit();
 		} catch (SQLException e) {
 			//Completar por el alumno			
-			
+			con.rollback();
 			logger.error(e.getMessage());
 			throw e;		
 
 		} finally {
-			/*A rellenar por el alumno, liberar recursos*/
+			/*Se liberan todos los recursos que sean necesarios*/
+			if(rs_sel_cliente!=null) rs_sel_cliente.close();
+			if(rs_sel_medico!=null) rs_sel_medico.close();
+			if(rs_sel_consulta!=null) rs_sel_consulta.close();
+			if(rs_sel_anulacion!=null) rs_sel_anulacion.close();
+			if(pst_sel_cliente!=null) pst_sel_cliente.close();
+			if(pst_sel_medico!=null) pst_sel_medico.close();
+			if(pst_sel_consulta!=null) pst_sel_consulta.close();
+			if(pst_sel_anulacion!=null) pst_sel_anulacion.close();
+			if(pst_ins_consulta!=null) pst_ins_consulta.close();
+			if(pst_upd_medico!=null) pst_upd_medico.close();
+			if(con!=null) con.close();			
 		}
 		
 		
 	}
-	
-	public static void anular_consulta(String m_NIF_cliente, String m_NIF_medico,  
-			Date m_Fecha_Consulta, Date m_Fecha_Anulacion, String motivo)
-			throws SQLException {
+
+	public static void anular_consulta(String m_NIF_cliente, String m_NIF_medico, Date m_Fecha_Consulta,
+			Date m_Fecha_Anulacion, String motivo) throws SQLException {
+
+		PoolDeConexiones pool = PoolDeConexiones.getInstance();
+		Connection con = null;
 		
-		PoolDeConexiones pool = PoolDeConexiones.getInstance();
-		Connection con=null;
-
-	
-		try{
+		try {
 			con = pool.getConnection();
+			con.setAutoCommit(false);
 			
 		} catch (SQLException e) {
-			//Completar por el alumno			
-			
+			// Completar por el alumno
+
 			logger.error(e.getMessage());
-			throw e;		
+			throw e;
 
 		} finally {
-			/*A rellenar por el alumno, liberar recursos*/
-		}		
+			/* A rellenar por el alumno, liberar recursos */
+		}
 	}
-	
-	public static void consulta_medico(String m_NIF_medico)
-			throws SQLException {
 
-				
+	public static void consulta_medico(String m_NIF_medico) throws SQLException {
+
 		PoolDeConexiones pool = PoolDeConexiones.getInstance();
-		Connection con=null;
+		Connection con = null;
 
-	
-		try{
+		try {
 			con = pool.getConnection();
-			
+
 		} catch (SQLException e) {
-			//Completar por el alumno			
-			
+			// Completar por el alumno
+
 			logger.error(e.getMessage());
-			throw e;		
+			throw e;
 
 		} finally {
-			/*A rellenar por el alumno, liberar recursos*/
-		}		
+			/* A rellenar por el alumno, liberar recursos */
+		}
 	}
-	
+
 	static public void creaTablas() {
 		ExecuteScript.run(script_path + "gestion_medicos.sql");
 	}
 
-	static void tests() throws SQLException{
+	static void tests() throws SQLException {
 		creaTablas();
-		
-		PoolDeConexiones pool = PoolDeConexiones.getInstance();		
-		
-		//Relatar caso por caso utilizando el siguiente procedure para inicializar los datos
-		
-		CallableStatement cll_reinicia=null;
+
+		PoolDeConexiones pool = PoolDeConexiones.getInstance();
+
+		// Relatar caso por caso utilizando el siguiente procedure para inicializar los
+		// datos
+
+		CallableStatement cll_reinicia = null;
 		Connection conn = null;
-		
+
 		try {
-			//Reinicio filas
+			// Reinicio filas
 			conn = pool.getConnection();
 			cll_reinicia = conn.prepareCall("{call inicializa_test}");
 			cll_reinicia.execute();
-		} catch (SQLException e) {				
-			logger.error(e.getMessage());			
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
 		} finally {
-			if (cll_reinicia!=null) cll_reinicia.close();
-			if (conn!=null) conn.close();
-		
-		}			
-		
+			if (cll_reinicia != null)
+				cll_reinicia.close();
+			if (conn != null)
+				conn.close();
+
+		}
+
 	}
 }
